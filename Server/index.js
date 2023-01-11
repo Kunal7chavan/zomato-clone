d693666dc386b6644b5662cef13d0e84dccd8600
rahulchavan3 <rahulchavan3030@gmail.com>
2023-01-11 22:37:05 +0530

    Completed user middleware for user Authentication

--- Server/index.js
+++ Server/index.js
@@ -1,30 +1,48 @@
+import express from "express";
+import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
 
-import express from 'express';
-import dotenv from 'dotenv';
+// Private route authorization config
+import privateRouteConfig from "./config/route.config";
 
 // Database connection
 import ConnectDB from "./database/connection";
 
 import Auth from "./api/auth";
+import Food from "./api/food";
+import Restaurant from "./api/restaurant";
+import User from "./api/user";
 
 dotenv.config();
 
+privateRouteConfig(passport);
+
 const zomato = express();
 
+// adding additional passport configuration
+
 zomato.use(express.json());
-zomato.get('/',(req,res)=>{
-    res.json({
-        message : 'Server is running',
-    })
-})
+zomato.use(session({ secret: "ZomatoApp" }));
+zomato.use(passport.initialize());
+zomato.use(passport.session());
+
+zomato.get("/", (req, res) => {
+  res.json({
+    message: "Server is running",
+  });
+});
 
 // /auth/signup
 zomato.use("/auth", Auth);
+zomato.use("/food", Food);
+zomato.use("/restaurant", Restaurant);
+zomato.use("/user", passport.authenticate("jwt", { session: false }), User);
 
 const PORT = 4000;
 
-zomato.listen(PORT, ()=>{
-      ConnectDB()
+zomato.listen(PORT, () => {
+  ConnectDB()
     .then(() => {
       console.log("Server is running !!!");
     })
@@ -32,5 +50,4 @@ zomato.listen(PORT, ()=>{
       console.log("Server is running, but database connection failed...");
       console.log(error);
     });
- 
 });
